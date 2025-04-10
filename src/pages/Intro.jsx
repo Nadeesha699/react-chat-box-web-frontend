040cc0acab89c6a88f40365e094a1744db7b5697
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-04-11 00:04:02 +0530

    saved

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,5 +1,42 @@
+import Logo from "../images/n_8970968.png";
+
 const Intro = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "tomato",
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "steelblue",
+          height: "80vh",
+          width: "70%",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div style={{ backgroundColor: "wheat", width: "50%" }}>
+          <img src={Logo} />
+          <input placeholder="Email" />
+          <input placeholder="User name" />
+          <input placeholder="Password" type="password" />
+          <button>Sign up</button>
+        </div>
+        <div style={{ backgroundColor: "darkorange", width: "50%" }}>
+          <img src={Logo} />
+          <input placeholder="User name" />
+          <input placeholder="Password" type="password" />
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
