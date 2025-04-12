3cd971bdd2d4e575ba5698964a6fd37e806d6c19
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-04-12 11:27:30 +0530

    saved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { CheckCheck, Edit, Search, Send, User } from "lucide-react";
+import { CheckCheck, Edit, Search, Send, User, User2 } from "lucide-react";
 import { useState } from "react";
 // https://dribbble.com/shots/23280048-Web-Chat-UI
 
@@ -143,8 +143,22 @@ const messaheData = [
   },
 ];
 
+const user = [
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+  { name: "Nadeesha" },
+];
+
 const Home = () => {
   const [searchText, setSearchText] = useState("");
+  const [visibleUserList, setVisibleUserList] = useState(false);
   return (
     <div className="home-container">
       <div className="home-message-list">
@@ -169,7 +183,14 @@ const Home = () => {
               onChange={(e) => setSearchText(e.target.value)}
             />
           </div>
-          <Edit onClick={()=>{alert("test")}} style={{cursor:"pointer"}}/>
+          <Edit
+            onClick={() => {
+              visibleUserList
+                ? setVisibleUserList(false)
+                : setVisibleUserList(true);
+            }}
+            style={{ cursor: "pointer" }}
+          />
         </div>
         <div className="scroll-container">
           {chatData
@@ -264,6 +285,61 @@ const Home = () => {
             }}
           />
           <Send />
+        </div>
+      </div>
+      <div
+        style={{
+          transition: "ease-in-out all 1s",
+          position: "fixed",
+          top: visibleUserList ? "10%" : "-100%",
+          left: "15%",
+          backgroundColor: "white",
+          boxShadow: "10px 10px 10px black",
+          borderRadius: "20px",
+          padding: "1%",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          height: "300px",
+        }}
+      >
+        <div className="home-message-list-1">
+          <Search />
+          <input
+            placeholder="search"
+            style={{
+              width: "100%",
+              borderStyle: "hidden",
+              backgroundColor: "transparent",
+              outline: "none",
+            }}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "5px",
+            overflow: "scroll",
+          }}
+        >
+          {user.map((e, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+              >
+                <User />
+                <label>{e.name}</label>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
